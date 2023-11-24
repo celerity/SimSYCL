ac817ce50850c6f4a118318c7cb9ea599d71ad52
Peter Thoman <petert@dps.uibk.ac.at>
2023-11-24 09:56:48 +0100

    Fix coordinate friend operator definitions

--- include/simsycl/detail/coordinate.hh
+++ include/simsycl/detail/coordinate.hh
@@ -45,12 +45,12 @@ class coordinate {
 
 #define SIMSYCL_DETAIL_DEFINE_COORDINATE_BINARY_COPY_LHS_OPERATOR(op)                                                  \
     friend constexpr Interface operator op(const Interface &lhs, const Interface &rhs) {                               \
-        Interface result;                                                                                              \
+        Interface result = make_interface_type();                                                                      \
         for(int d = 0; d < Dimensions; ++d) { result[d] = lhs.m_values[d] op rhs.m_values[d]; }                        \
         return result;                                                                                                 \
     }                                                                                                                  \
     friend constexpr Interface operator op(const Interface &lhs, const size_t &rhs) {                                  \
-        Interface result;                                                                                              \
+        Interface result = make_interface_type();                                                                      \
         for(int d = 0; d < Dimensions; ++d) { result[d] = lhs.m_values[d] op rhs; }                                    \
         return result;                                                                                                 \
     }
@@ -99,7 +99,7 @@ class coordinate {
 
 #define SIMSYCL_DETAIL_DEFINE_COORDINATE_BINARY_COPY_RHS_OPERATOR(op)                                                  \
     friend constexpr Interface operator op(const size_t &lhs, const Interface &rhs) {                                  \
-        Interface result;                                                                                              \
+        Interface result = make_interface_type();                                                                      \
         for(int d = 0; d < Dimensions; ++d) { result[d] = lhs op rhs.m_values[d]; }                                    \
         return result;                                                                                                 \
     }
@@ -125,7 +125,7 @@ class coordinate {
 
 #define SIMSYCL_DETAIL_DEFINE_COORDINATE_UNARY_COPY_OPERATOR(op)                                                       \
     friend constexpr Interface operator op(const Interface &rhs) {                                                     \
-        Interface result;                                                                                              \
+        Interface result = make_interface_type();                                                                      \
         for(int d = 0; d < Dimensions; ++d) { result[d] = op rhs[d]; }                                                 \
         return result;                                                                                                 \
     }
@@ -148,7 +148,7 @@ class coordinate {
 
 #define SIMSYCL_DETAIL_DEFINE_COORDINATE_UNARY_POSTFIX_OPERATOR(op)                                                    \
     friend constexpr Interface operator op(Interface &lhs, int) {                                                      \
-        Interface result = lhs;                                                                                        \
+        Interface result = lhs = make_interface_type();                                                                \
         for(int d = 0; d < Dimensions; ++d) { lhs[d] op; }                                                             \
         return result;                                                                                                 \
     }
@@ -160,6 +160,10 @@ class coordinate {
 
   private:
     size_t m_values[Dimensions];
+
+    // interface type construction helper to use in friend operators
+    // (because friendship is not transitive)
+    static Interface make_interface_type() { return {}; }
 };
 
 } // namespace simsycl::detail
