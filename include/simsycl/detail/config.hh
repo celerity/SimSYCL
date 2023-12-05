68c642ab5cfa46d47844bc7fcfe1854a3745d4c1
Peter Thoman <petert@dps.uibk.ac.at>
2023-12-05 17:45:31 +0100

    Start testing sub group ops

--- include/simsycl/detail/config.hh
+++ include/simsycl/detail/config.hh
@@ -6,7 +6,21 @@ namespace simsycl::detail {
 
 class config {
   public:
-    static constexpr uint32_t max_sub_group_size = 32;
+    inline static uint32_t max_sub_group_size = 32;
+};
+
+template <typename T>
+class configure_temporarily {
+  public:
+    configure_temporarily(T &to_configure, T new_value) : m_to_configure(to_configure) {
+        m_old_value = to_configure;
+        to_configure = new_value;
+    }
+    ~configure_temporarily() { m_to_configure = m_old_value; }
+
+  private:
+    T &m_to_configure;
+    T m_old_value;
 };
 
 } // namespace simsycl::detail
