b5e2ea3f6fe38c43a9e731f6c179d38dfdfa3d1e
tapplencourt <tapplencourt@anl.gov>
2025-07-15 20:08:59 +0000

    Implementation of SYCL_KHR_QUEUE_FLUSH

--- src/simsycl/queue.cc
+++ src/simsycl/queue.cc
@@ -69,4 +69,6 @@ context queue::get_context() const { return state().context; }
 
 device queue::get_device() const { return state().device; }
 
+void queue::khr_flush() const {}
+
 } // namespace simsycl::sycl
