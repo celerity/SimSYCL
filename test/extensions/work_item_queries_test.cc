3b5ae850169875538836b82e253f3653f62bd381
Peter Thoman <peter@ph3.at>
2025-09-01 14:24:05 +0200

    Add error reporting unit test for SYCL_KHR_WORK_ITEM_QUERIES

--- test/extensions/work_item_queries_test.cc
+++ test/extensions/work_item_queries_test.cc
@@ -1,21 +1,23 @@
+#include "simsycl/sycl/queue.hh"
 #include <simsycl/sycl.hh>
 
 #include <catch2/catch_template_test_macros.hpp>
 #include <catch2/catch_test_macros.hpp>
 #include <catch2/generators/catch_generators.hpp>
+#include <catch2/matchers/catch_matchers_string.hpp>
 
+using Catch::Matchers::ContainsSubstring;
 
 using namespace simsycl;
 
 TEMPLATE_TEST_CASE_SIG(
     "work item queries are correct if supported", "[khr][work_item_queries]", ((int Dims), Dims), 1, 2, 3) {
-
-    #if SIMSYCL_ENABLE_SYCL_KHR_WORK_ITEM_QUERIES
+#if SIMSYCL_ENABLE_SYCL_KHR_WORK_ITEM_QUERIES
 
     sycl::range<Dims> global_range;
     sycl::range<Dims> local_range;
     for(int d = 0; d < Dims; ++d) {
-        const int s = d+1;
+        const int s = d + 1;
         global_range[d] = s * (2 + s);
         local_range[d] = 2 + s;
     }
@@ -43,9 +45,36 @@ TEMPLATE_TEST_CASE_SIG(
         })
         .wait();
 
-    for(size_t i = 0; i < global_range.size(); ++i) { CAPTURE(i); CHECK(visited[i]); }
+    for(size_t i = 0; i < global_range.size(); ++i) {
+        CAPTURE(i);
+        CHECK(visited[i]);
+    }
 
-    #else // SIMSYCL_ENABLE_SYCL_KHR_WORK_ITEM_QUERIES
+#else  // SIMSYCL_ENABLE_SYCL_KHR_WORK_ITEM_QUERIES
     SKIP("SYCL_KHR_WORK_ITEM_QUERIES not enabled");
-    #endif // SIMSYCL_ENABLE_SYCL_KHR_WORK_ITEM_QUERIES
+#endif // SIMSYCL_ENABLE_SYCL_KHR_WORK_ITEM_QUERIES
+}
+
+TEST_CASE("work item queries provide useful errors", "[khr][work_item_queries]") {
+#if SIMSYCL_ENABLE_SYCL_KHR_WORK_ITEM_QUERIES
+
+    // outside of everything
+    REQUIRE_THROWS_WITH(sycl::khr::this_nd_item<1>(), ContainsSubstring("state 'this_nd_item' is not available"));
+    REQUIRE_THROWS_WITH(sycl::khr::this_group<1>(), ContainsSubstring("state 'this_group' is not available"));
+    REQUIRE_THROWS_WITH(sycl::khr::this_sub_group(), ContainsSubstring("state 'this_sub_group' is not available"));
+
+    // in a non-nd parallel for
+    sycl::queue{}.submit([&](sycl::handler &cgh) {
+        cgh.parallel_for(sycl::range{1}, [=](sycl::item<1>) {
+            const char *test_str
+                = "Make sure that the query originated from a kernel launched with a sycl::nd_range argument";
+            CHECK_THROWS_WITH(sycl::khr::this_nd_item<1>(), ContainsSubstring(test_str));
+            CHECK_THROWS_WITH(sycl::khr::this_group<1>(), ContainsSubstring(test_str));
+            CHECK_THROWS_WITH(sycl::khr::this_sub_group(), ContainsSubstring(test_str));
+        });
+    });
+
+#else  // SIMSYCL_ENABLE_SYCL_KHR_WORK_ITEM_QUERIES
+    SKIP("SYCL_KHR_WORK_ITEM_QUERIES not enabled");
+#endif // SIMSYCL_ENABLE_SYCL_KHR_WORK_ITEM_QUERIES
 }
